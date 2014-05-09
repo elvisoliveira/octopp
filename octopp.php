7f35ef560d293342adefa3db1109182c826a8ddb
Elvis Oliveira <contato@elvisoliveira.com.br>
2014-05-08 23:56:28 -0300

    ajust css query configuration, convert to XML

--- octopp.php
+++ octopp.php
@@ -3,5 +3,10 @@
 require_once 'vendor/autoload.php';
 
 use OctoPP\Parse;
+use LSS\Array2XML;
 
-var_dump(Parse::getFirstPage());
+$xml = Array2XML::createXML('articles', Parse::getFirstPage());
+
+print $xml->saveXML();
+
+
--- src/OctoPP/Parse.php
+++ src/OctoPP/Parse.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Parser the recieved information in order to deal with
  * the external information a proper way.
@@ -6,38 +7,73 @@
  * @author     Elvis Oliveira <http://github.com/elvisoliveira>
  * @license    MIT License
  */
+
 namespace OctoPP {
-    
+
     class Parse {
+
+        // config: extract text
+        private static $text = array(
+            'points' => 'p.post-meta a.point span.badge-item-love-count',
+            'comments' => 'p.post-meta a.comment'
+        );
+        // config: extract attr
+        private static $attr = array(
+            'id' => array('', 'data-entry-id'),
+            'next' => array('a.badge-next-post-entry', 'data-entry-key'),
+            'content' => array('div.post-container img', 'src'),
+            'title' => array('div.post-container img', 'alt')
+        );
+
         /**
          * Get all the articles from the first page.
-         * @return array
+         * @return type
          */
         public static function getFirstPage() {
             // get all the articles
             $articles = htmlqp('http://9gag.com', 'article');
+
+            $nodes = array();
+
             // loop the entries
-            foreach($articles as $id => $article) {
-                $nodes[$id] = self::getArticle($article->attr('data-entry-id'));
+            foreach ($articles as $id => $article) {
+                // get information about entries
+                $nodes['article'][$id] = self::getArticle($article->attr('data-entry-id'));
             }
-            
+
+            // return articles
             return $nodes;
-            
         }
+
         /**
+         * Get the parameters from the original source page.
+         * @param string $id
+         * @return array
          */
         public static function getArticle($id) {
             // get node from original page
             $article = htmlqp("http://9gag.com/gag/{$id}", "article");
-            
+
             // get and store all information about that single article
-            $node['id'] = trim(qp($article)->attr('data-entry-id')); # id
-            $node['points'] = trim(qp($article)->find('p.post-meta a.point span.badge-item-love-count')->text()); # points
-            $node['comments'] = trim(qp($article)->find('p.post-meta a.comment')->text()); # comments
-            $node['next'] = trim(qp($article)->find('a.badge-next-post-entry')->attr('data-entry-key')); # next ID
-            $node['image'] = trim(qp($article)->find('div.post-container img')->attr('src')); # image URL
-            
+            foreach (self::$text as $id => $row) {
+                // set params as a node
+                $node[$id] = trim(qp($article)->find($row)->text());
+            }
+
+            // get and store all information about that single article
+            foreach (self::$attr as $id => $row) {
+                // set params as a node
+
+                if (empty($row[0])) {
+                    $node[$id] = qp($article)->attr($row[1]);
+                } else {
+                    $node[$id] = qp($article)->find($row[0])->attr($row[1]);
+                }
+            }
+
             return $node;
         }
+
     }
+
 }
